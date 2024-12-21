29ad2359b6a042d3e46db4bb77af99ff7dd20711
Jethro-Cortez <jpcortez@mymail.mapua.edu.ph>
2024-12-21 23:28:02 +0800

    Delete test_stats.py

--- test_stats.py
+++
@@ -1,7 +0,0 @@
-import stats
-
-numbers = [1, 2, 3, 4, 4, 5, 5]
-
-print("Mean:", stats.mean(numbers))   # Output: Mean: 3.4285714285714284
-print("Median:", stats.median(numbers))  # Output: Median: 4
-print("Mode:", stats.mode(numbers))   # Output: Mode: [4, 5]
